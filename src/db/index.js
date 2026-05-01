bbd0c97395371bc6f7bbacdb67c55b25c0f24b35
Nency2006 <nency417@gmail.com>
2026-05-01 13:22:25 +0530

    next commit

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+const connectDb = async ()=>{
+    try {
+        const connectioninstatnce = await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
+        console.log(`\n MongoDb connected !! DB HOST :${connectioninstatnce.connection.host} `);
+        
+    } catch (error) {
+        console.error("MONGODB CONNECTION ERROR : ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDb;
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+import connectDb from "./db";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDb()
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+
+( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
+        app.on("error", (error)=>{
+            console.log("ERROR : ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listing on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR : ", error)
+        throw error
+    }
+} )()
+*/
