e05b724dc3903aef0b9212965060d787007720c5
Nency2006 <nency417@gmail.com>
2026-05-01 15:01:38 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -6,30 +6,18 @@ dotenv.config({
 })
 
 connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running on ${process.env.PORT || 8000}`);
+        
+    });
+})
+.catch((err)=>{
+    console.log("MONGODB connection FAILD !!", err);
+    
+})
 
 
 
 
 
-
-/*
-import express from "express";
-const app = express()
-
-( async ()=>{
-    try {
-        await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
-        app.on("error", (error)=>{
-            console.log("ERROR : ", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, ()=>{
-            console.log(`App is listing on port ${process.env.PORT}`)
-        })
-    } catch (error) {
-        console.error("ERROR : ", error)
-        throw error
-    }
-} )()
-*/
