8ef77a792805f8659f8eb3015905b4da55592336
Nency2006 <nency417@gmail.com>
2026-05-05 12:56:05 +0530

    add routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import {Router} from "express";
-import { registerUser, loginUser, logOutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    registerUser,
+    loginUser, 
+    logOutUser, 
+    refreshAccessToken, 
+    changeCurrentUserPassword,
+    getCurrentUser,
+    updateAccountDeatils,
+    updateUserAvatar,
+    updateUserCoverImage
+} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -28,6 +38,16 @@ router.route("/login").post(
 router.route("/logout").post(verifyJWT, logOutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/update-password").put(verifyJWT, changeCurrentUserPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").put(verifyJWT, updateAccountDeatils)
+
+router.route("/update-avatae").put(verifyJWT, upload.single({name : "avatar"}), updateUserAvatar)
+
+router.route("/update-coverImage").put(verifyJWT, upload.single({name : "coverImage"}) ,updateUserCoverImage)
 
 
 export default router; 
